199160bb43ba4b029597260b8a2ea7c439605b9d
monolok <antoinebertin@me.com>
2015-03-01 14:37:59 +0100

    scaffold images and config search

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 
+root 'home#index'
+
+  resources :images do 
+    collection do 
+      get 'search'
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
